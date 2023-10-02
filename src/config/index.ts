2a8108dbfa9e6ad2732316d636f8671e607f6725
Truong Nguyen <truongnguyenptn@gmail.com>
2023-10-02 12:49:35 +0700

    change domain according to ssl

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ dotenv.config()
 export const config = {
   domain: process.env.APP_HOST ?? 'localhost',
   port: process.env.APP_PORT ?? 3000,
-  apiUrl: process.env.FILMATRON_SERVER_URL ?? 'http://45.77.182.167/graphql',
+  apiUrl: process.env.FILMATRON_SERVER_URL ?? 'https://45.77.182.167/graphql',
   admin: {
     // wallet address that admin will sign transaction when film maker action create a collection for their NFT collection
     publickKey: process.env.ADMIN_PUBLICK_KEY ?? '89Fh4QKhCEJ5rC1Bf4utchfmqPNejYTfjoW6VxDL8YqB'

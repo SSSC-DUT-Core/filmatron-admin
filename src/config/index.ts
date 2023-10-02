d1934a70deb71507bd250e24d0113bb569053dc8
Truong Nguyen <truongnguyenptn@gmail.com>
2023-10-02 21:37:13 +0700

    update url

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,12 +2,12 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-
+const API_URL = 'http://45.77.182.167/graphql'
 
 export const config = {
   domain: process.env.APP_HOST ?? 'localhost',
   port: process.env.APP_PORT ?? 3000,
-  apiUrl: process.env.FILMATRON_SERVER_URL ?? 'https://45.77.182.167/graphql',
+  apiUrl: process.env.FILMATRON_SERVER_URL ?? API_URL,
   admin: {
     // wallet address that admin will sign transaction when film maker action create a collection for their NFT collection
     publickKey: process.env.ADMIN_PUBLICK_KEY ?? '89Fh4QKhCEJ5rC1Bf4utchfmqPNejYTfjoW6VxDL8YqB'

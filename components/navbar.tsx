fe0f514644c91e5fdbbcaad4165790b2de596ab1
Truong Nguyen <truongnguyenptn@gmail.com>
2023-10-13 16:23:45 +0700

    feat: fix build failure

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,21 @@
+"use client"
 import { MainNav } from "@/components/main-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button"
 
 const Navbar = () => {
-
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+      setLoggedIn(Boolean(localStorage?.getItem('access_token')));
+  })
   return ( 
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <MainNav className="mx-6" />
+        
         <div className="ml-auto flex items-center space-x-4">
-          <ThemeToggle />
+        {isLoggedIn ? (<ThemeToggle />) : (<Button>Login</Button>)}
         </div>
       </div>
     </div>
